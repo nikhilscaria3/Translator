c7b2145c8cc294b49ff08e4221646cfef03b87a6
nikhilscaria3 <125331845+nikhilscaria3@users.noreply.github.com>
2023-10-25 01:21:40 +0530

    add

--- server/index.js
+++ server/index.js
@@ -30,6 +30,11 @@ app.post("/translate", async (req, res) => {
   }
 });
 
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
