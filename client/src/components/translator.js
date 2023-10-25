d2c86a47d4dec427f261741ba3a8f303bd1407f5
nikhilscaria3 <125331845+nikhilscaria3@users.noreply.github.com>
2023-10-25 08:36:08 +0530

    bug fix

--- client/src/components/translator.js
+++ client/src/components/translator.js
@@ -3,7 +3,7 @@ import { franc } from "franc";
 import { faCopy, faXmarkCircle } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMicrophone, faVolumeUp } from "@fortawesome/free-solid-svg-icons";
-import axios from "../utils/baseapi"
+import axios from '../utils/baseapi'; // Import Axios
 
 const languages = [
   { code: "af", name: "Afrikaans" },
